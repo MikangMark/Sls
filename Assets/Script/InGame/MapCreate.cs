a51a28b07fcca8c72b2de5701a41c1cbdd375c41
MikangMark <kimjh741963@hanmail.net>
2023-03-13 16:53:42 +0900

    맵작업중2

--- Assets/Script/InGame/MapCreate.cs
+++ Assets/Script/InGame/MapCreate.cs
@@ -9,7 +9,7 @@ public class MapCreate : MonoBehaviour
     //방의종류 8가지 미지 상인 보물 휴식 일반적 엘리트 보스 빈방
     //0.빈방, 1.일반적, 2.엘리트, 3.휴식, 4.상인, 5.미지, 6.보물, 7.보스
     //랜덤생성되는것은 보스를 제외한 6가지
-    //0층은 일반적으로 고정, 14층은 휴식으로 고정, 15층은 보스방으로고정 1개로고정
+    //0층은 일반적으로 고정, 13층은 휴식으로 고정, 14층은 보스방으로고정 1개로고정
 
     /*
      1. 맵이 만들어지는 과정
@@ -19,71 +19,107 @@ public class MapCreate : MonoBehaviour
         각 경로는 y축으로 1만큼 증가할때 x좌표는 -1부터 1까지 랜덤하게 변하면서 올라감.
         이걸 총 6번 반복한 다음에 경로상에 있는 모든 격자점들을 모은게 결과적인 맵임.
      */
-    static int row = 16;
-    static int col = 6;
+    static int row = 15;
+    static int col = 7;
+    int createCount_Op = 0;
+    int createRoomCount = 0;
     int[,] map = new int[row, col];
-    public GameObject room_Parent;
+    public GameObject room_p;
+    public int poolSize = row * col;
+    public List<GameObject> objectPool;
+    public GameObject roomObj_Parents;
+    static public int seed = Environment.TickCount;
+    //0층은 빈방과 일반적만나오게
+    //8층은 보물 고정
+    //14층은 휴식만나오도록
+    //15층은 보스 한곳만나오도록
+    //8가지의 방목록중 랜덤방은 0~5까지
 
-    void Create_Frame()
+    /*
+     미리 최대크기의 맵(방)을 만들고 그것들을 오브젝트 풀링기법사용
+     
+     */
+    void Start()
     {
-        int seed = DateTime.Now.Millisecond;
-        System.Random rand = new System.Random(seed);
-        for (int i = 0; i < row; i++)
+        
+        //Create_RoomObj(0, 0);
+        SetObjectPool();
+
+        UseObjectPool();
+    }
+    void UseObjectPool()
+    {
+        GameObject room = GetObjectFromPool();
+        room.SetActive(true);
+    }
+    int RandNum(int min, int max)
+    {
+        System.Random random = new System.Random(seed);
+        int randnum = random.Next(max - min) + min;
+        return randnum;
+    }
+    int[] RandNumNotSame(int min, int max, int count)
+    {
+        int[] randarr = new int[count];
+        System.Random random = new System.Random(seed);
+        int randnum = 0;
+        for (int i = 0; i < count; i++)
         {
             if (i == 0)
             {
-                for (int j = 0; j < col; j++)
-                {
-                    map[i, j] = rand.Next(2);//0층은 빈방과 일반적만나오게
-                }
+                randarr[i] = random.Next(max - min) + min;
             }
-            else if (i == 8)
+            else if (i > 0)
             {
-                for (int j = 0; j < col; j++)
-                {
-                    map[i, j] = 5;//8층은 보물 고정
-                }
+                
             }
-            else if (i == 14)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    map[i, j] = 3;//14층은 휴식만나오도록
-                }
-            }
-            else if (i == 15)
-            {
-                map[i, 3] = 7;//15층은 보스 한곳만나오도록
-            }
-            else
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    map[i, j] = rand.Next(6);//8가지의 방목록중 랜덤방은 0~5까지
-                }
-            }
-
+            
         }
+        
+        return randarr;
     }
-    void Create_RoomObj(int row, int col)
-    {
-        GameObject room_Btn = new GameObject("Room[" + row + "][" + col + "]");
-        room_Btn.AddComponent<Button>();
-        room_Btn.transform.SetParent(room_Parent.transform);
-        room_Btn.GetComponent<Transform>().position = new Vector2(room_Parent.transform.position.x, room_Parent.transform.position.y);
-        //GameObject textP = GameObject.Find("Room");
-        //GameObject textC = textP.transform.GetChild(0).gameObject;
-        //Destroy(textC);
-    }
-    void Start()
-    {
-        //Create_Frame();
-        Create_RoomObj(0, 0);
-    }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void SetObjectPool()
+    {
+        objectPool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(room_p, Vector3.zero, Quaternion.identity);
+            obj.SetActive(false);
+            obj.transform.SetParent(roomObj_Parents.transform);
+            obj.name = "Room[" + createCount_Op / 7 + "][" + createCount_Op % 7 + "]";
+            createCount_Op++;
+            objectPool.Add(obj);
+        }
+    }
+    public GameObject GetObjectFromPool()
+    {
+        foreach (GameObject obj in objectPool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+
+        // 사용 가능한 객체가 없을 경우 새로 생성 / 버그아닌이상 이쪽으로는 안올듯
+        GameObject newObj = Instantiate(room_p, Vector3.zero, Quaternion.identity);
+        newObj.SetActive(true);
+        newObj.transform.SetParent(roomObj_Parents.transform);
+        newObj.name = "Room[" + createCount_Op / 7 + "][" + createCount_Op % 7 + "]";
+        createCount_Op++;
+        objectPool.Add(newObj);
+        return newObj;
+    }
+
+    // 사용이 완료된 객체를 오브젝트 풀로 반환하는 함수
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 }
