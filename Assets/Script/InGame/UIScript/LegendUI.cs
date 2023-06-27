f4545e0f1746c5014d0e32e8fc7ac86db0d36127
MikangMark <kimjh741963@hanmail.net>
2023-06-27 13:31:00 +0900

    추가작업시작

---
+++ Assets/Script/InGame/UIScript/LegendUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LegendUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public MapNode.ROOMVALUE room_Type;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
