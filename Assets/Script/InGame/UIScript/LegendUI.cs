ff7c47314b9a21ea07e5d842f1abab9f6948f65c
JangHan Kim <kimjh741963@hanmail.net>
2023-07-03 13:31:32 +0900

    구글시트 변경전

--- Assets/Script/InGame/UIScript/LegendUI.cs
+++ Assets/Script/InGame/UIScript/LegendUI.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class LegendUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public MapNode.ROOMVALUE room_Type;
 
+    Room[] all;
+    private void Start()
+    {
+        all = FindObjectsOfType<Room>();
+        Debug.Log(room_Type);
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
+        for(int i = 0; i < all.Length; i++)
+        {
+            if(room_Type == all[i].node.roomType)
+            {
+                all[i].gameObject.GetComponent<Image>().color = Color.white;
+            }
+        }
+
         
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        for (int i = 0; i < all.Length; i++)
+        {
+            if (room_Type == all[i].node.roomType)
+            {
+                all[i].gameObject.GetComponent<Image>().color = Color.black;
+                
+            }
+        }
     }
 }
